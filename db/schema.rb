6f6f4808bdf0350c2ddee312437ac2dcf72a5781
emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-09-15 00:10:12 +0300

    check the migrations timestamps and update the database.yml file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_164953) do
+ActiveRecord::Schema.define(version: 2020_09_14_164859) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -38,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_09_14_164953) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
